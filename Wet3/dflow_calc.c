173f626fb88d81a1acc264a60f1c6e9db56d840b
Unknown <nogigo@gmail.com>
2018-06-05 00:06:55 +0300

    fix getInstDeps

--- Wet3/dflow_calc.c
+++ Wet3/dflow_calc.c
@@ -9,7 +9,7 @@
 #define ENTRYID -1
 
 typedef struct cmd_node cmdNode;
-typedef struct dp_graph *DPGraph;
+typedef struct dp_graph DPGraph;
 
 struct cmd_node{
 	int id;
@@ -31,56 +31,71 @@ ProgCtx analyzeProg(const unsigned int opsLatency[],  InstInfo progTrace[], unsi
 	if (!opsLatency || !progTrace || numOfInsts==0) {
 		return PROG_CTX_NULL;
 	}
-	DPGraph graph = malloc(sizeof(DPGraph));
+	DPGraph* graph = (DPGraph*)malloc(sizeof(DPGraph));
 	if (!graph) {
 		return PROG_CTX_NULL;
 	}
-	graph->cmd_pArray = malloc(numOfInsts*sizeof(cmdNode));
+	graph->cmd_pArray = (cmdNode*)malloc(numOfInsts*sizeof(cmdNode));
 	if (!(graph->cmd_pArray)) {
 		free(graph);
 		return PROG_CTX_NULL;
 	}
 	graph->numOfInsts = numOfInsts;
+	graph->depth = 0;
 
-	graph->entry->depth = 0;
-	graph->entry->duration = 0;
-	graph->entry->dp1 = NULL;
-	graph->entry->dp2 = NULL;
-	graph->entry->depth = 0;
-	graph->entry->id = ENTRYID;
-	graph->entry->info = NULL;
+	graph->entry.depth = 0;
+	graph->entry.duration = 0;
+	graph->entry.dp1 = NULL;
+	graph->entry.dp2 = NULL;
+	graph->entry.depth = 0;
+	graph->entry.id = ENTRYID;
+	graph->entry.info = NULL;
 
 	for (int i = 0 ; i < numOfInsts ; i++) {
 		graph->cmd_pArray[i].id = i;
-		graph->cmd_pArray[i].info = malloc(sizeof(InstInfo*));
+		graph->cmd_pArray[i].info = (InstInfo*)malloc(sizeof(InstInfo));
+		if (!(graph->cmd_pArray[i].info)) {
+			for (int j=0 ; j<i-1 ; j++ ) {
+				free(graph->cmd_pArray[j].info);
+			}
+			free(graph->cmd_pArray);
+			free(graph);
+			return PROG_CTX_NULL;
+		}
 		graph->cmd_pArray[i].info->dstIdx = progTrace[i].dstIdx;
 		graph->cmd_pArray[i].info->opcode = progTrace[i].opcode;
 		graph->cmd_pArray[i].info->src1Idx = progTrace[i].src1Idx;
 		graph->cmd_pArray[i].info->src2Idx = progTrace[i].src2Idx;
+
 		graph->cmd_pArray[i].duration = opsLatency[progTrace[i].opcode];
 
-		bool dp1_taken = false;
 		graph->cmd_pArray[i].dp2 = NULL;
+		graph->cmd_pArray[i].dp1 = NULL;
+		graph->cmd_pArray[i].depth = 0;
 		for (int j = i-1 ; j >= 0; j--) {
-			if(graph->cmd_pArray[i].info->src1Idx == graph->cmd_pArray[j].info->dstIdx ||
-							graph->cmd_pArray[i].info->src2Idx == graph->cmd_pArray[j].info->dstIdx) {
-				if (!dp1_taken) {
+			if(graph->cmd_pArray[i].info->src1Idx == graph->cmd_pArray[j].info->dstIdx) {
+				if (graph->cmd_pArray[i].dp1 == NULL) {
 					graph->cmd_pArray[i].dp1 = &(graph->cmd_pArray[j]);
-					dp1_taken = true;
-					graph->cmd_pArray[i].depth = graph->cmd_pArray[j]->depth+graph->cmd_pArray[j]->duration;
-				} else {
-					graph->cmd_pArray[i].dp2 = &(graph->cmd_pArray[j]);
-					if ((graph->cmd_pArray[i].depth) < (graph->cmd_pArray[j]->depth+graph->cmd_pArray[j]->duration)) {
-						graph->cmd_pArray[i].depth = graph->cmd_pArray[j]->depth+graph->cmd_pArray[j]->duration;
+					if ((graph->cmd_pArray[i].depth) < (graph->cmd_pArray[j].depth+graph->cmd_pArray[j].duration)) {
+						graph->cmd_pArray[i].depth = graph->cmd_pArray[j].depth+graph->cmd_pArray[j].duration;
 					}
-					break;
 				}
 			}
+			if (graph->cmd_pArray[i].info->src2Idx == graph->cmd_pArray[j].info->dstIdx) {
+				if (graph->cmd_pArray[i].dp2 == NULL) {
+					graph->cmd_pArray[i].dp2 = &(graph->cmd_pArray[j]);
+					if ((graph->cmd_pArray[i].depth) < (graph->cmd_pArray[j].depth+graph->cmd_pArray[j].duration)) {
+						graph->cmd_pArray[i].depth = graph->cmd_pArray[j].depth+graph->cmd_pArray[j].duration;
+					}
+				}
+
+			}
 		}
-		if (!dp1_taken) {
+		if (graph->cmd_pArray[i].dp1 == NULL && graph->cmd_pArray[i].dp2 == NULL) {
 			graph->cmd_pArray[i].dp1 = &(graph->entry);
-			graph->cmd_pArray[i].dp2 = NULL;
-			graph->cmd_pArray[i].depth = 0;
+		}
+		if (graph->depth < (graph->cmd_pArray[i].depth+graph->cmd_pArray[i].duration)) {
+			graph->depth = graph->cmd_pArray[i].depth+graph->cmd_pArray[i].duration;
 		}
 	}
 
@@ -89,35 +104,40 @@ ProgCtx analyzeProg(const unsigned int opsLatency[],  InstInfo progTrace[], unsi
 
 void freeProgCtx(ProgCtx ctx) {
 	if (ctx != NULL) {
-		for (int i=0 ; i<((DPGraph)ctx)->numOfInsts ; i++ ) {
-			free(((DPGraph)ctx)->cmd_pArray[i].info);
+		for (int i=0 ; i<((DPGraph*)ctx)->numOfInsts ; i++ ) {
+			free(((DPGraph*)ctx)->cmd_pArray[i].info);
 		}
-		free(((DPGraph)ctx)->cmd_pArray);
+		free(((DPGraph*)ctx)->cmd_pArray);
 		free(ctx);
 	}
 }
 
 int getInstDepth(ProgCtx ctx, unsigned int theInst) {
-	if (ctx == NULL || theInst >= ((DPGraph)ctx)->numOfInsts) {
+	if (ctx == NULL || theInst >= ((DPGraph*)ctx)->numOfInsts) {
 		return -1;
 	}
-    return (((DPGraph)ctx)->cmd_pArray[theInst]->depth);
+    return (((DPGraph*)ctx)->cmd_pArray[theInst].depth);
 }
 
 int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2DepInst) {
-	if (ctx == NULL || theInst >= ((DPGraph)ctx)->numOfInsts)
+	if (ctx == NULL || theInst >= ((DPGraph*)ctx)->numOfInsts)
 		return -1;
-	if (((DPGraph)ctx)->cmd_pArray[theInst]->dp1->id == ENTRYID) {
+	if (((DPGraph*)ctx)->cmd_pArray[theInst].dp1 == NULL) {
+		*src1DepInst = ENTRYID;
+		*src2DepInst = ((DPGraph*)ctx)->cmd_pArray[theInst].dp2->id;
+		return 0;
+	}
+	else if (((DPGraph*)ctx)->cmd_pArray[theInst].dp1->id == ENTRYID) {
 		*src1DepInst = ENTRYID;
 		*src2DepInst = ENTRYID;
 		return 0;
 	}
 	else {
-		*src1DepInst = ((DPGraph)ctx)->cmd_pArray[theInst]->dp1->id;
-		if (((DPGraph)ctx)->cmd_pArray[theInst]->dp2 == NULL)
+		*src1DepInst = ((DPGraph*)ctx)->cmd_pArray[theInst].dp1->id;
+		if (((DPGraph*)ctx)->cmd_pArray[theInst].dp2 == NULL)
 			*src2DepInst = ENTRYID;
 		else
-			*src2DepInst = ((DPGraph)ctx)->cmd_pArray[theInst]->dp2->id;;
+			*src2DepInst = ((DPGraph*)ctx)->cmd_pArray[theInst].dp2->id;
 		return 0;
 	}
     return -1;
@@ -127,7 +147,7 @@ int getProgDepth(ProgCtx ctx) {
 	if (ctx == NULL)
 		return 0;
 	else
-		return ((DPGraph)ctx)->depth;
+		return ((DPGraph*)ctx)->depth;
 }
 
 
